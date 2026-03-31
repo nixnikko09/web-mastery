206398c3256a17953c3008ffee5450d67a45af08
Nikko Del Rosario <nikkodelrosario999@gmail.com>
2026-03-31 15:00:44 +0800

    Functions

--- script.js
+++ script.js
@@ -9,10 +9,16 @@ console.log(`I study ${course}`);
 console.log(`I study at ${school}`);
 console.log(`I want to become a ${dreamJob}`);
 
-let score = 60;
+let score = 85;
 if (score >= 75) {
     console.log("Passed")
 } else {
     console.log("Failed")
 }
 
+function introduce() {
+    console.log("Hello my name is Nikko");
+    console.log("I study IT");
+    console.log("I want to become a developer");
+}
+introduce();
